03f086affeb1535d82efe43e50414bddc3880743
qian <863540560@qq.com>
2018-08-13 17:05:24 +0800

    歌手列表完成

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="song-list">
+    <li class="song-item" v-for="(item, index) in songList" :key="index">
+      <h2 class="name">{{item.name}}</h2>
+      <p class="desc">{{item.singer}} · {{item.album}}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    songList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+.song-list
+  .song-item
+    margin-bottom 30px
+    color #fff
+    font-size 14px
+    .name, .desc
+      width 100%
+      margin-top 10px
+      overflow hidden
+      text-overflow ellipsis
+      white-space nowrap
+    .desc
+      color rgba(255,255,255,0.3)
+</style>
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="music-list">
     <div class="head">
-      <div class="bank">
-        <i class="icon-bank"></i>
+      <div class="back" @click="back">
+        <i class="icon-back"></i>
       </div>
       <h1 class="title">{{title}}</h1>
     </div>
     <div class="container">
-      <div class="bg-img">
-        <img :src="bgImg" alt="">
+      <div class="bg-img" :style="bgStyle" ref="bgImg">
+        <div class="play-box" ref="playBox" v-if="songList.length">
+          <i class="icon-play"></i>
+          <span>随机播放全部</span>
+        </div>
+        <div class="filter" ref="filter"></div>
       </div>
-      <div class="play-box">
-        <i class="icon-play"></i>
-        <span>随机播放全部</span>
-      </div>
-      <ul class="song-list">
-        <li class="song-item" v-for="(item, index) in songList" :key="index">
-          <h2 class="name">{{item.name}}</h2>
-          <p class="desc">{{item.singer}}</p>
-        </li>
-      </ul>
+      <div class="bg-layer" ref="layer"></div>
+      <scroll :data='songList' 
+              :probeType='probeType' 
+              :listenScroll='listenScroll' 
+              @posY='scroll'
+              ref='scrollWrapper'>
+        <div class="song-list-wrapper">
+          <song-list :songList='songList'></song-list>
+        </div>
+        <div class="loading-container">
+          <loading></loading>
+        </div>
+      </scroll>
     </div>
   </div>
 </template>
 
 <script>
+import scroll from 'base/scroll/scroll'
+import songList from 'base/song-list/song-list'
+import loading from 'base/loading/loading'
+import {prefixStyle} from 'api/dom.js'
+const HEAD_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 export default {
   props: {
     title: {
@@ -39,13 +53,143 @@ export default {
         return []
       }
     }
+  },
+  data() {
+    return {
+      probeType: 3,
+      listenScroll: true,
+      posY: 0
+    }
+  },
+  mounted() {
+    this.h = this.$refs.bgImg.clientHeight
+    this.minTranslateY = -this.h + HEAD_HEIGHT
+    this.$refs.scrollWrapper.$el.style.top = `${this.h}px`
+    this.$refs.scrollWrapper.$el.style.height = `${document.documentElement.clientHeight - this.h}px`
+  },
+  computed: {
+    bgStyle() {
+      return `background-image: url(${this.bgImg})`
+    }
+  },
+  methods: {
+    back() {
+      this.$router.go(-1)
+    },
+    scroll(pos) {
+      this.posY = pos.y
+    }
+  },
+  watch: {
+    posY(newY) {
+      let max = Math.max(this.minTranslateY, newY)
+      this.$refs.layer.style[transform] = `translate3d(0,${max}px,0)`
+      let zIndex = 0
+      let scale = 1
+      let blur = 20
+      const percent = Math.abs(newY / this.h)
+      // 判断列表滑动到顶部 设置顶部图片高度以及z-index
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImg.style.paddingTop = 0
+        this.$refs.bgImg.style.height = HEAD_HEIGHT + 'px'
+        this.$refs.playBox.style.display = 'none'
+      } else {
+        this.$refs.bgImg.style.paddingTop = '70%'
+        this.$refs.bgImg.style.height = 0
+        this.$refs.playBox.style.display = ''
+      }
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(percent * 20, blur)
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      }
+      this.$refs.bgImg.style.zIndex = zIndex
+      this.$refs.bgImg.style[transform] = `scale(${scale})`
+    }
+  },
+  components: {
+    songList,
+    scroll,
+    loading
   }
 }
 </script>
 
 <style scoped lang='stylus' rel='stylesheet/stylus'>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
 .music-list
-    .head
-      display flex
-
+  position: fixed
+  z-index: 100
+  top: 0
+  left: 0
+  bottom: 0
+  right: 0
+  .head
+    position absolute
+    top 0px
+    left 0
+    width 100%
+    color #fff
+    line-height 40px
+    z-index 50
+    .back
+      position absolute
+      top 0px
+      left 6px
+      .icon-back
+        display inline-block
+        padding 10px
+        font-size: $font-size-large-x
+        color: $color-theme
+    .title
+      font-size 18px
+      text-align center
+  .container
+    height 100%
+    .bg-layer
+      position relative
+      height 100%
+      background #222
+    .bg-img
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 70%
+      transform-origin: top
+      background-size: cover
+      .play-box
+        position: absolute;
+        bottom: 20px;
+        left 50%;
+        transform translateX(-50%)
+        z-index: 50;
+        padding: 6px 20px;
+        border-radius 100px
+        border: 1px solid #ffcd32;
+        color: #ffcd32;
+        .icon-play
+          vertical-align middle
+          margin-right 2px
+        span
+          font-size 12px
+          vertical-align middle
+      .filter
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        background: rgba(7, 17, 27, 0.4)
+    .scroll-wrapper
+      position absolute
+      top 0
+      left 0
+      width 100%
+      overflow inherit
+      .song-list-wrapper
+        padding 20px 30px
 </style>
